ee66afb2efca4bad97ac40fd7c7f17b75fb36344
xuhanlin <xuhanlin1@126.com>
2025-01-14 15:02:30 +0800

    第二次提交

---
+++ learning/Task1/course02.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+# 加载环境变量
+load_dotenv()
+# 从环境变量中读取api_key
+api_key = os.getenv('ZISHU_API_KEY')
+base_url = "http://43.200.7.56:8008/v1"
+chat_model = "glm-4-flash"
